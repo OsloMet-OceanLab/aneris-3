0df52a60c1312c0e169594cda8b453705f184e9d
Giorgio Salvemini <56552241+Giorgio68@users.noreply.github.com>
2024-11-08 10:56:56 +0100

    Update uvc.py
    Added scheduling

--- scripts/uvc.py
+++ scripts/uvc.py
@@ -4,11 +4,15 @@ author: @rambech
 
 import gpiod as GPIO
 from time import sleep
+from scripts import configuration
+from apscheduler.schedulers.background import BackgroundScheduler
 
-# Setup PWM pin
+
+# Setup GPIO pin
 UVC_pin = 25
 chip = GPIO.Chip('gpiochip4')
 uvc_line = chip.get_line(UVC_pin)
+
 
 def on():
     try:
@@ -17,12 +21,14 @@ def on():
     finally:
         uvc_line.release()
 
+
 def off():
     try:
         uvc_line.request(consumer="LED", type=GPIO.LINE_REQ_DIR_OUT)
         uvc_line.set_value(0)
     finally:
         uvc_line.release()
+
 
 def test():
     try:
@@ -36,4 +42,32 @@ def test():
 
 
 def scheduled():
-    pass
+    """
+    Enables schedules runtime for the lights
+    """
+    
+    CONFIG = configuration.get()["uvc"]
+    
+    start_time = CONFIG["time"]
+    start_hour, start_min = start_time.split(":")
+    duration = CONFIG["duration"]
+    duty_cycle = CONFIG["duty_cycle"]
+    period = CONFIG["period"]
+
+    def run_uvc():
+        on_time = period * duty_cycle
+        off_time = period - on_time
+        repeat = duration / period
+
+        while repeat > 0:
+            on()
+            sleep(on_time)
+            off()
+            sleep(off_time)
+            repeat -= 1
+
+    scheduler = BackgroundScheduler()
+    scheduler.configure(timezone=utc)
+    scheduler.add_job(run_uvc, trigger='cron', hour=start_hour, minute=start_min)
+
+    scheduler.start()
